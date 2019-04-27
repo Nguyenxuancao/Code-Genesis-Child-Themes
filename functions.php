4c20c0c842d585bf51e36446688876a6c8386ac0
Nguyenxuancao <49977669+Nguyenxuancao@users.noreply.github.com>
2019-04-27 09:16:49 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -205,3 +205,41 @@ function genesis_child_comments_gravatar( $args ) {
 	return $args;
 
 }
+// Updates theme settings on reset.
+add_filter( 'genesis_theme_settings_defaults', 'genesis_child_theme_defaults' );
+function genesis_child_theme_defaults( $defaults ) {
+
+	$defaults['blog_cat_num']              = 5;
+	$defaults['content_archive']           = 'full';
+	$defaults['content_archive_limit']     = 380;
+	$defaults['content_archive_thumbnail'] = 1;
+	$defaults['image_alignment']           = 'alignleft';
+	$defaults['image_size']                = 'thumbnail';
+	$defaults['posts_nav']                 = 'prev-next';
+	$defaults['site_layout']               = 'content-sidebar';
+
+	return $defaults;
+
+}
+// Updates theme settings on activation.
+add_action( 'after_switch_theme', 'genesis_child_theme_setting_defaults' );
+function genesis_child_theme_setting_defaults() {
+
+	if( function_exists( 'genesis_update_settings' ) ) {
+
+		genesis_update_settings( array(
+			'blog_cat_num'              => 5,
+			'content_archive'           => 'full',
+			'content_archive_limit'     => 380,
+			'content_archive_thumbnail' => 1,
+			'image_alignment'           => 'alignleft',
+			'image_size'                => 'thumbnail',
+			'posts_nav'                 => 'prev-next',
+			'site_layout'               => 'content-sidebar',
+		) );
+
+	}
+
+	update_option( 'posts_per_page', 5 );
+
+}
